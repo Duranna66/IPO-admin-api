aa3bbb864b3ba3e6fed576be68b7b3c4bcd5be3a
Николаев Иван Сергеевич <ivan.kushnarev.04@list.ru>
2024-03-17 13:18:53 +0300

    feat: add features

--- src/main/java/com/arminik/nm/admin/Application.java
+++ src/main/java/com/arminik/nm/admin/Application.java
@@ -12,17 +12,8 @@ import java.util.Arrays;
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) throws IOException, InterruptedException {
-		String var1 = "as";
-		String var2 = "asdasd";
-		String path = "scripts/deploy.sh";
-		ProcessBuilder pb = new ProcessBuilder(path, var1);
-		Process p = pb.start();
-		p.waitFor();
-		for(var x : p.getInputStream().readAllBytes()) {
-			System.out.print((char)x);
-		}
-//		SpringApplication.run(Application.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 }
